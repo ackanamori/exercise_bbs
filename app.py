30e7fd0f21b0ad921f6658332a907fb306029ed8
Miyui <s19t006@stu.kagawa-u.ac.jp>
2021-06-17 20:42:25 +0900

    datetimeっぽいものつけました

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # splite3をimportする
-import sqlite3
+import sqlite3 ,datetime
 # flaskをimportしてflaskを使えるようにする
 from flask import Flask , render_template , request , redirect , session
 # appにFlaskを定義して使えるようにしています。Flask クラスのインスタンスを作って、 app という変数に代入しています。
@@ -103,11 +103,14 @@ def add():
     user_id = session['user_id']
     # フォームから入力されたアイテム名の取得
     comment = request.form.get("comment")
+    datetime= request.form.get("datetime")
+    datetime.now()
+    print(now)
     conn = sqlite3.connect('service.db')
     c = conn.cursor()
     # DBにデータを追加する
-    #aoiue
-    c.execute("insert into bbs values(null,?,?)", (user_id, comment))
+    # status.created_at
+    c.execute("insert into bbs values(null,?,?)", (user_id, comment, datetime))
     conn.commit()
     conn.close()
     return redirect('/bbs')
